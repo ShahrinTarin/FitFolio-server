4c3fd37f32d78c94218fb1b6e01daf68b7872047
ShahrinTarin <rahmanshahrintarin@gmail.com>
2026-04-01 13:44:21 +0600

    add ci/cd pipeline

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ app.use((req, res, next) => {
   next();
 });
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4wteejr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri =
+  process.env.MONGODB_URI ||
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4wteejr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
